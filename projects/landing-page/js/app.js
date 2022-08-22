5e77835232cccc0ae6e191cd153024a2ee455fb9
Hagarbelal <108432761+Hagarbelal@users.noreply.github.com>
2022-08-23 00:04:15 +0200

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -39,24 +39,47 @@
 */
 
 // build the nav
+const ul=document.getElementById("navbar__list");
+const sections=Array.from(document.querySelectorAll("section"));
 
 
-// Add class 'active' to section when near top of viewport
+function createListItem(){
+    for ( let section of sections) {
+        sectionName=section.getAttribute('dataset.nav');
+        sectionLink=section.getAttribute('id');
+        listItem=document.createElement("li") ;
+        listItem.innerHTML=`<a href="#${sectionLink}" data-nav="${sectionLink}"  class="menu__link">${section.dataset.nav}</a>`
+     
+
+        ul.appendChild(listItem);
+    }
+
+    }createListItem();
+    
+    //using getBoundingClientRect to styling for active
+    window.onscroll=function(){
+       document.querySelectorAll("section").forEach(function (active){
+       if(
+        active.getBoundingClientRect().top>=-400 &&
+        active.getBoundingClientRect().top<=150
+       ){
+        active.classList.add("your-active-class");
+       }else{
+        active.classList.remove("your-active-class");
+       }
+       });
+    };
+    //using arrow function to scroll section when click an any section in the navigation bar
+    ul.addEventListener("click",(toSection) => {
+        toSection.preventDefault();
+            if(toSection.target.dataset.nav){
+                document
+                .getElementById(`${toSection.target.dataset.nav}`)
+               .scrollIntoView({behavior:"smooth"});
+                
+            }
+        });
 
 
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
